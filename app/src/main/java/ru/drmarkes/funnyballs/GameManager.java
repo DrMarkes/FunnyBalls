cf23a7f537ee543f1dae1642084a96029597b1a4
DrMarkes <markovandrew@yandex.ru>
2015-12-21 14:40:43 +0700

    класс GameManager получает от CanvasView и сохраняет размеры экрана. Выводит главный шарик в центр экрана.

--- app/src/main/java/ru/drmarkes/funnyballs/GameManager.java
+++ app/src/main/java/ru/drmarkes/funnyballs/GameManager.java
@@ -9,8 +9,14 @@ import android.graphics.Paint;
 public class GameManager {
     private MainBall mainBall;
     private Paint paint;
+    private CanvasView canvasView;
+    private static int width;
+    private static int height;
 
-    public GameManager() {
+    public GameManager(CanvasView canvasView, int width, int height) {
+        this.canvasView = canvasView;
+        GameManager.width = width;
+        GameManager.height = height;
         initMainBall();
         initPaint();
     }
@@ -22,7 +28,7 @@ public class GameManager {
     }
 
     private void initMainBall() {
-        mainBall = new MainBall(200, 500);
+        mainBall = new MainBall(width / 2, height / 2);
     }
 
     public void onDraw(Canvas canvas) {
